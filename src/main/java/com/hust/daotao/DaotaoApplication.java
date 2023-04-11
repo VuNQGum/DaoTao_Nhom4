54565ca31cf61502d32d6daf6e30e05fe394aa15
VuNQGum <nguyenquangvu05042001@gmail.com>
2023-04-11 21:49:41 +0700

    first commit

---
+++ src/main/java/com/hust/daotao/DaotaoApplication.java
@@ -0,0 +1,13 @@
+package com.hust.daotao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaotaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaotaoApplication.class, args);
+    }
+
+}
